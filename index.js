6ff0f7ef6cc4cef2b2f5b34872ddeadd4ba778eb
Gabriela Justice <gabynicole98@gmail.com>
2017-06-05 14:39:13 -0400

    cleaned up code and added comments in the index.js

--- index.js
+++ index.js
@@ -2,20 +2,20 @@ const personForm = document.querySelector('#personForm')
 
 personForm.addEventListener('submit', handleSubmit)
 
-function handleSubmit(ev){
+function handleSubmit(ev){ //stop the form from submitting and call the functions to change things
   ev.preventDefault()
   const f = ev.target
   hChange(f)
   pChange(f)
 }
 
-function hChange(f){
+function hChange(f){ //change the h1 to include the name and age
   name = f.personName.value
   age = f.personAge.value
   document.querySelector('h1').textContent = 'Profile: ' + name + ', ' + age
 }
 
-function pChange(f){
+function pChange(f){ //change the empty paragraph to have the name and age with the selected color text.
   color = f.personColor.value
   name = f.personName.value
   age = f.personAge.value
