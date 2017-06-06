1d1d0eb2958ae0f48baf7092f3240a9ac99ea489
Gabriela Justice <gabynicole98@gmail.com>
2017-06-06 10:22:38 -0400

    added a list of the form values to details using the templates

--- index.js
+++ index.js
@@ -7,25 +7,30 @@ function handleSubmit(ev){ //stop the form from submitting and call the function
   const f = ev.target
   hChange(f)
   pChange(f)
-  const details = document.querySelector('#details')
-  const name = f.personName.value
-  //details.innerHTML = `<em>${name}</em>`
-  const em = document.createElement('em')
-  em.textContent = name
-  details.appendChild(em)
 }
 
 function hChange(f){ //change the h1 to include the name and age
   name = f.personName.value
   age = f.personAge.value
-  document.querySelector('h1').textContent = 'Profile: ' + name + ', ' + age
+  document.querySelector('h1').textContent = `Profile: ${name}, ${age}`
 }
 
 function pChange(f){ //change the empty paragraph to have the name and age with the selected color text.
   color = f.personColor.value
   name = f.personName.value
   age = f.personAge.value
-  document.querySelector('.results').textContent = 'Hello, ' + name + '! You are ' + age + ' years old!'
   document.querySelector('.results').style.color = color
+
+  const details = document.querySelector('#details')
+  const em = document.createElement('em')
+  const colorDiv = `<div style="background-color:${color}; width:7rem; height:3rem;"></div>`
+
+  details.innerHTML = `
+  <ul>
+    <li>Name: ${name}</li>
+    <li>Favorite Color: ${colorDiv}</li>
+    <li>Age: ${age}</li>
+  </ul>
+  `
 
 }
